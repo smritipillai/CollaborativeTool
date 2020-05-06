3fd5e1c89576f582af651a0f844c2cf4853f09c2
vagrant <vagrant@ubuntu-18.localdomain>
2020-05-06 13:54:19 +0530

    Added File Upload and added support for multiple server

--- Square/square/app.py
+++ Square/square/app.py
@@ -2,9 +2,10 @@ import json
 import os
 import uuid
 from collections import deque
+from pathlib import Path
 from datetime import datetime
 
-from flask import Flask, jsonify, redirect, render_template, request, url_for, session, abort
+from flask import Flask, jsonify, redirect, render_template, request, url_for, session, abort,send_from_directory
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
 from .model import *
@@ -15,6 +16,9 @@ app.config['SECRET_KEY'] = uuid.uuid4().hex
 socketio = SocketIO(app)
 
 server_list = ServerList()
+
+TRASH = "/tmp/files"
+Path(TRASH).mkdir(parents=True, exist_ok=True)
 
 # Testing
 # server = Server("test","test")
@@ -46,6 +50,7 @@ def createServer():
         session['user'] = user_name
         session['user-key'] = key
         session['server'] = server_key
+        session['channel-name'] = "public"
 
         return redirect(url_for("server",server_key=server_key))
 
@@ -78,23 +83,42 @@ def joinServer(server_key):
             session['user'] = user.name
             session['user-key'] = user.key
             session['server'] = server_key
+            session['channel-name'] = "public"
             return redirect(url_for("server",server_key=server_key))
         elif request.method == "GET":
             return render_template('join_server.html',server_name=server.name)
     abort(404)
 
-
-@app.route('/server/<string:server_key>/<string:channel_name>')
-def joinChannel(server_key,channel_name):
+@app.route('/uploader', methods = ['POST'])
+def uploadFile():
+    server_key = session.get('server','')
     server = server_list.getServer(server_key)
-    channel = server.getChannel(channel_name)
-    if channel:
-        session['channel'] = channel_name
-        join_room(channel_name)
-        return redirect(url_for("join_server",server_key=server_key))
+    if server:
+        print(session)
+        user = session['user']
+        channel_name = session['channel-name']
+        if request.method == 'POST':
+            f = request.files['file']
+            file_name = uuid.uuid4().hex
+            message_text = "<a href=\"{}/{}\">{}</a>".format(request.base_url,file_name,file_name)
 
+            message = Message(user, message_text)
+            channel = server.getChannel(channel_name)
+            if channel:
+                f.save(TRASH + "/" + file_name)
+                channel.addMessage(message)
+                socketio.emit('message', {'user': user,'message': message_text}, room=(channel_name + server_key))
+
+            return redirect(url_for("server",server_key=server_key))
     abort(404)
 
+@app.route('/uploader/<string:filename>')
+def downloadFile(filename):
+    print(filename)
+    try:
+        return send_from_directory(TRASH, filename=filename, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
 
 @app.route('/')
 def index():
@@ -108,36 +132,39 @@ def createChannel(data):
     if server:
         channel = Channel(channel_name)
         server.addChannel(channel)
-        emit('channel-list',server.getChannelNames())
+        emit('channel-list',server.getChannelNames(),room=server_key)
 
 @socketio.on("join")
 def Join(data):
     server_key = session.get('server','')
     server = server_list.getServer(server_key)
+    join_room(server_key)
     if server:
-        emit('channel-list',server.getChannelNames())
+        emit('channel-list',server.getChannelNames(),room=server_key)
         user_name = server.getUserNames()
-        emit('user-list', user_name)
+        emit('user-list', user_name,room=server_key)
 
 @socketio.on('join-channel')
 def joinChannel(data):
-    server_key = session['server']
+    server_key = session.get('server',"")
     server = server_list.getServer(server_key)
-    channels = parse_channel(session.get('channelName',""))
-    for channel in channels:
-        leave_room(channel)
-
     if server:
-        channel_name = data['channelName']
-        channel = server.getChannel(channel_name)
-        if channel:
-            session['channelName'] = channel_name
-            channels = parse_channel(channel_name)
-            for c in channels:
-                join_room(c)
-            emit('channel-list',server.getChannelNames())
-            messages = [{"user": x.name, "message": x.text} for x in channel.getMessages()]
-            emit('load-message',messages)
+        channels = parse_channel(session.get('channel-name',""))
+        for channel in channels:
+            if channel:
+                leave_room(channel + server_key)
+
+        if server:
+            channel_name = data['channelName']
+            channel = server.getChannel(channel_name)
+            if channel:
+                session['channel-name'] = channel_name
+                channels = parse_channel(channel_name)
+                for c in channels:
+                    join_room(c + server_key)
+                emit('channel-list',server.getChannelNames(),room=server_key)
+                messages = [{"user": x.name, "message": x.text} for x in channel.getMessages()]
+                emit('load-message',messages,room=server_key)
 
 @socketio.on('message')
 def handle_message(data):
@@ -152,7 +179,7 @@ def handle_message(data):
         channel = server.getChannel(channel_name)
         if channel:
             channel.addMessage(message)
-            emit('message', {'user': user,'message': message_text}, room=channel_name)
+            emit('message', {'user': user,'message': message_text}, room=(channel_name + server_key))
 
 if __name__ == '__main__':
 
